38cb5e1d9572e6f59f75e4191c1cc1be26748160
Rossatoo <matheusrossato2002@hotmail.com>
2022-03-21 19:30:00 -0300

    Tipos de variaveis
    Algoritmo para demonstrar os tipos de variaveis e atribuição de valores para elas

---
+++ Aula 21.03/Tipos_Variaveis.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+main() {
+	int x = 2, y = 4, z = 10, s;
+	float a = 3, c = 4.23, d = 3.14;
+	char i = 'hi', j = 'a';
+	
+	printf(" O x = %i, y = %i e o z = %i ", x,y,z);
+	printf("\n Os floats a = %f, c = %f e o d = %f ", a,c,d);
+	printf("\n Os chars i = %c e o j = %c ", i,j);
+	
+	getch();
+	
+}
