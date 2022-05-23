b862a93c7dcaaef71bb186dc003a6bdbc86fe323
Rossatoo <102099800+Rossatoo@users.noreply.github.com>
2022-05-23 20:27:45 -0300

    Update questao05.c

--- Lista 16.05 - FOR/questao05.c
+++ Lista 16.05 - FOR/questao05.c
@@ -1,16 +1,16 @@
-/*Imprima uma tabela de conversão de polegadas para centímetros, de 1 a 20. Considere que Polegada = Centímetro * 2,54.*/
+/*Imprima uma tabela de conversÃ£o de polegadas para centÃ­metros, de 1 a 20. Considere que Polegada = CentÃ­metro * 2,54.*/
 
 #include <conio.h>
 #include <stdio.h>
 
 main(){
 	int i;
-	float polegada;
+	float cm;
 	
 	for(i=1; i<=20; i++){
-		polegada = i * 2.54;
+		cm = i / 2.54;
 		
-		printf	("%d x 2,54 = %.2f\n", i,polegada);
+		printf	("%d x 2,54 = %.2f\n", i,cm);
 	}
 	
 	
